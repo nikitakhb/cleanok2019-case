3b93d3bbf35ffc75721d1f3bbb1e4c86ce6e674f
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2019-01-16 00:02:11 +1000

    rename an app and add dependencies

--- cleanok/cleanok/settings.py
+++ cleanok/cleanok/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Vladivostok'
 
 USE_I18N = True
 
--- cleanok/cleanokapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CleanokappConfig(AppConfig):
-    name = 'cleanokapp'
