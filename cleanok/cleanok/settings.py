22b20d8073554735ef3520f5dcc4e5c010c2bed7
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2019-01-31 00:22:54 +1000

    fix static files

--- cleanok/cleanok/settings.py
+++ cleanok/cleanok/settings.py
@@ -139,7 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static/'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
