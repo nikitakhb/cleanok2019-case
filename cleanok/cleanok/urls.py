79f6c0c17be47eed92580e425d742e7c807dfad4
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2019-01-31 00:53:24 +1000

    fix static files path

--- cleanok/cleanok/urls.py
+++ cleanok/cleanok/urls.py
@@ -32,4 +32,8 @@ urlpatterns = [
     path('partnership/', include('partners.urls')),
     path('advices/', include('advices.urls')),
     path('contacts/', include('contacts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
