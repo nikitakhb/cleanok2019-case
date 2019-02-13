ca38eed373ad144f642dead0968679004241e2bb
Nikita Semenov <nikita_khb@mail.ru>
2019-02-13 10:51:50 +1000

    Create ViewSet for FeedBackMessage

--- cleanok/feedback/views.py
+++ cleanok/feedback/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import FeedBackMessage
+from .serializers import FeedBackMessageSerializer
+
+
+class FeedBackMessageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+
+    queryset = FeedBackMessage.objects.all()
+    serializer_class = FeedBackMessageSerializer
