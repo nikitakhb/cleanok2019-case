86d54c57d2191a3808200719ea109f3f1aecbd89
Nikita Semenov <nikita_khb@mail.ru>
2019-02-13 10:40:59 +1000

    Create model FeedBack

--- cleanok/feedback/models.py
+++ cleanok/feedback/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class FeedBack(models.Model):
+    """ FeedBack model """
+    first_name = models.CharField('Имя', max_length=32)
+    last_name = models.CharField('Фамилия', max_length=32)
+    message = models.TextField('Сообщение', max_length=512)
+    date = models.DateTimeField('Дата/Время', auto_now=True)
+
+    def __str__(self):
+        return self.message[:50]
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
