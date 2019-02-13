e893b915466c04c9b875dbcdd7c2c6443c3b1b4b
Nikita Semenov <nikita_khb@mail.ru>
2019-02-13 10:55:52 +1000

    Create admin model for FeedBackMessage model

--- cleanok/feedback/admin.py
+++ cleanok/feedback/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FeedBackMessage
+
+
+@admin.register(FeedBackMessage)
+class FeedBackMessageAdmin(admin.ModelAdmin):
+    list_display = ('date', '__str__')
